13a263123e4350850c09fe5b141750a95daafca2
ZhylinskayaKseniya <k1090898@gmail.com>
2017-06-20 19:53:39 +0300

    Update OutputNumbers.java

--- OutputNumbers.java
+++ OutputNumbers.java
@@ -1,10 +1,10 @@
 class OutputNumbers {
   public static void main(String[] args) {
-    for (int i = 0; i <= 100; i++){
+    for (int i = 0; i <= 100; i++) {
       if(i % 3 == 0) {
-        System.out.print("3*" + i/3 + "\n");
+        System.out.println("3*" + i/3);
       } else {
-          System.out.print(i + " ");
+          System.out.println(i);
       }    
     }
   }
